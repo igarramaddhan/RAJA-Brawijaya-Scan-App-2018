040c7d30ea5757cc4eaacc1cb855e6f5a0ed4241
Igar Ramaddhan <igarramaddhan@gmail.com>
2018-07-23 22:58:10 +0700

    form input component

---
+++ src/components/FormInput.js
@@ -0,0 +1,48 @@
+import React, { Component, Ref } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+const styles = StyleSheet.create({
+	inputContainer: {
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		marginVertical: 5
+	}
+});
+type Props = {
+	placeholder: String,
+	viewRef: Ref<View>,
+	onChangeText: Function,
+	onSubmitEditing: Function,
+	inputRef: Ref<TextInput>,
+	returnKeyType: String,
+	returnKeyLabel: String
+};
+type State = {};
+export default class FormInput extends Component<Props, State> {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		const {
+			viewRef,
+			placeholder,
+			onChangeText,
+			onSubmitEditing,
+			inputRef,
+			...props
+		} = this.props;
+		return (
+			<Animatable.View ref={viewRef} style={styles.inputContainer}>
+				<TextInput
+					{...props}
+					ref={inputRef}
+					padding={10}
+					placeholder={placeholder}
+					onChangeText={onChangeText}
+					onSubmitEditing={onSubmitEditing}
+				/>
+			</Animatable.View>
+		);
+	}
+}
